98039261cda96dcfc86c2306076d9345ca4c34d7
Sebastian Safari <ssebs@ymail.com>
2024-01-25 17:37:06 -0800

    redesign

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// main function
 void main() {
-  runApp(const MyApp());
+  runApp(const TiprHome());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// Home widget with theme info, appbar, etc.
+class TiprHome extends StatelessWidget {
+  const TiprHome({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,52 +17,46 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade700),
         useMaterial3: true,
       ),
-      home: const SafeArea(child: TiprHome()),
-    );
-  }
-}
-
-class TiprHome extends StatelessWidget {
-  const TiprHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-          title: const Text("Tipr"),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade700,
+            title: const Text(
+              "Tipr",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          body: const CalcForm(),
         ),
-        body: const CalcForm());
-  }
-}
-
-class CalcForm extends StatefulWidget {
-  const CalcForm({super.key});
-
-  @override
-  State<CalcForm> createState() => _CalcFormState();
-}
-
-class _CalcFormState extends State<CalcForm> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController tipController = TextEditingController();
-
-  var tipOptions = [12, 15, 18, 20];
-
-  double billAmount = 0.0;
-  int tipPercentage = 15;
-
-  var tipAmount = 0.0;
-  var totalAmount = 0.0;
-
-  /// alert will snow a snackbar
-  void alert(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
       ),
     );
   }
+}
+
+enum TipOption {
+  fifteen(value: 15, label: "15%"),
+  eighteen(value: 18, label: "18%"),
+  twenty(value: 20, label: "20%"),
+  twentytwo(value: 22, label: "22%");
+
+  const TipOption({required this.value, required this.label});
+  final num value;
+  final String label;
+}
+
+/// Calculator form state + functionality + rendering
+class _CalcFormState extends State<CalcForm> {
+  // final _formKey = GlobalKey<FormState>();
+  TipOption tipO = TipOption.fifteen;
+  final TextEditingController tipController = TextEditingController();
+
+  // user will change these
+  double billAmount = 0.0;
+  num tipPercentage = 15;
+
+  // these will be calculated
+  double tipAmount = 0.0;
+  double totalAmount = 0.0;
 
   // calculate the tip from the tipPercentage, billAmount.
   // updates tipAmount and totalAmount
@@ -69,161 +64,202 @@ class _CalcFormState extends State<CalcForm> {
     setState(() {
       tipAmount = billAmount * (tipPercentage / 100);
       totalAmount = tipAmount + billAmount;
+
+      // todo: implement "nearest rounded totalAmount"
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    bool isFirstGo = true;
-    tipController.addListener(() {
-      final String item = tipController.value.text;
+  /// sets the bill amount, then calculates
+  void _setBillAmount(String value) {
+    // Make sure value is a number
+    if (value.isEmpty) {
+      return;
+    }
 
-      //check if num
-      if (item != "" && !isFirstGo) {
-        try {
-          // calculate tip + total
-          tipPercentage = int.parse(item);
-          _calculate();
-        } catch (e) {
-          // _formKey.currentState?.reset();
-          tipController.value = const TextEditingValue(text: "15");
-        }
-      }
+    try {
+      double dValue = double.parse(value);
+
+      // Then update billAmount & calc
+      setState(() {
+        billAmount = dValue;
+        _calculate();
+      });
+    } catch (e) {
+      alert(context, "$value is not a number!");
+    }
+  }
+
+  /// sets the tip %, then calculates
+  void _setTipPercentage(TipOption option) {
+    if (option.value == 0) {
+      return;
+    }
+
+    // Update tipPercentage & calc
+    setState(() {
+      tipPercentage = option.value;
+      _calculate();
+      tipO = option;
     });
-    isFirstGo = false;
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
 
-    return Center(
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Bill amount
-            SizedBox(
-              width: 400,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: TextFormField(
-                      initialValue: "",
-                      onChanged: (value) => setState(() {
-                        //check if num
-                        if (value != "") {
-                          try {
-                            // calculate tip + total
-                            billAmount = double.parse(value);
-                            _calculate();
-                          } catch (e) {
-                            alert(context, "$value is not a number");
-                            _formKey.currentState?.reset();
-                          }
-                        }
-                      }),
-                      autofocus: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        label: Text("Bill \$"),
-                      ),
-                      keyboardType: TextInputType.number,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          alert(context, "Enter something");
-                        }
-                        return null;
-                      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        // Bill amount box
+        Center(
+          child: SizedBox(
+            width: 400,
+            child: Column(
+              children: [
+                // Bill amount textbox
+                SizedBox(
+                  width: 200,
+                  child: TextField(
+                    onChanged: _setBillAmount,
+                    keyboardType: TextInputType.number,
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.attach_money),
+                      hintText: "Bill Amount",
                     ),
                   ),
-
-                  // Tip %
-                  SizedBox(
-                    width: 100,
-                    child: DropdownMenu<int>(
-                      initialSelection: 15,
-                      controller: tipController,
-                      label: const Text("Tip %"),
-                      dropdownMenuEntries: [
-                        ...tipOptions.map((int tpl) {
-                          return DropdownMenuEntry<int>(
-                            value: tpl,
-                            label: "$tpl",
-                          );
-                        }),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(
-                    width: 10, // custom HACK
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
+          ),
+        ),
 
-            const SizedBox(
-              height: 40,
-            ),
-
-            // Display amounts if not 0
-            if (tipAmount != 0)
-              Center(
-                child: Container(
-                  width: 416,
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.greenAccent[700],
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(8),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SizedBox(
-                        width: 200,
-                        child: Column(
-                          children: [
-                            Text(
-                              "Tip Amount",
-                              style: textTheme.headlineSmall,
-                            ),
-                            Text(
-                              "\$${tipAmount.toStringAsFixed(2)}",
-                              style: textTheme.headlineLarge,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        width: 200,
-                        child: Column(
-                          children: [
-                            Text(
-                              "Total Amount",
-                              style: textTheme.headlineSmall,
-                            ),
-                            Text(
-                              "\$${totalAmount.toStringAsFixed(2)}",
-                              style: textTheme.headlineLarge,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+        // Tip percentages
+        Center(
+          child: SizedBox(
+            width: 400,
+            child: Column(children: [
+              // "tip"
+              const Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Tip %",
+                  style: TextStyle(
+                    fontSize: 24,
                   ),
                 ),
               ),
-          ],
+
+              // tip option buttons
+              SegmentedButton<TipOption>(
+                segments: <ButtonSegment<TipOption>>[
+                  ButtonSegment<TipOption>(
+                    value: TipOption.fifteen,
+                    label: Text(TipOption.fifteen.label),
+                  ),
+                  ButtonSegment<TipOption>(
+                    value: TipOption.eighteen,
+                    label: Text(TipOption.eighteen.label),
+                  ),
+                  ButtonSegment<TipOption>(
+                    value: TipOption.twenty,
+                    label: Text(TipOption.twenty.label),
+                  ),
+                  ButtonSegment<TipOption>(
+                    value: TipOption.twentytwo,
+                    label: Text(TipOption.twentytwo.label),
+                  ),
+                ],
+                selected: <TipOption>{tipO},
+                onSelectionChanged: (Set<TipOption> p0) {
+                  _setTipPercentage(p0.first);
+                  tipO = p0.first;
+                },
+                style: ButtonStyle(
+                  textStyle: MaterialStatePropertyAll<TextStyle>(
+                    theme.textTheme.bodyLarge!,
+                  ),
+                  padding: const MaterialStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.all(20),
+                  ),
+                ),
+              )
+            ]),
+          ),
         ),
-      ),
+
+        // Calculated values
+        Center(
+          child: Container(
+            padding: const EdgeInsets.all(25),
+            width: 400,
+            color: Colors.green,
+            child: DefaultTextStyle(
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    // tip amount
+                    Column(
+                      children: [
+                        const Text("Tip amount"),
+                        Row(
+                          children: [
+                            const Icon(Icons.attach_money, color: Colors.white),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                tipAmount.toStringAsFixed(2),
+                                style: const TextStyle(fontSize: 24),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+
+                    // bill total
+                    Column(
+                      children: [
+                        const Text("Bill total"),
+                        Row(
+                          children: [
+                            const Icon(Icons.attach_money, color: Colors.white),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                totalAmount.toStringAsFixed(2),
+                                style: const TextStyle(fontSize: 24),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                )),
+          ),
+        ),
+
+        // Reset btn
+      ],
     );
   }
+}
+
+/// Calculator form skel
+class CalcForm extends StatefulWidget {
+  const CalcForm({super.key});
+
+  @override
+  State<CalcForm> createState() => _CalcFormState();
+}
+
+/// alert will snow a snackbar
+void alert(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+    ),
+  );
 }
